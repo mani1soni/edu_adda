1fd913e7039f5268caf6d8a34c73a76e3927155b
Shane Rees <rees.shane@gmail.com>
2019-10-01 19:57:06 +0930

    Basic Function

--- python/hello.py
+++ python/hello.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env-python3
 
 print("Hello all, Ready for conributions")
+
+
+# Basic Functions by @shaggs
+
+'''def means define function, add is the name of the function
+A,B are the peramiters'''
+
+
+def add(a, b):
+    # now we make a string called answer and this will add the peramiters a and b
+    answer = a + b
+    print(answer)
+
+
+# call our Function
+add(1, 2)
