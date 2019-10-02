be2a06954b7d624661e2a5c50da8212f459d6ae0
Shane Rees <rees.shane@gmail.com>
2019-10-02 12:35:45 +0930

    Update lists.py

--- python/lists.py
+++ python/lists.py
@@ -1,25 +1,27 @@
 """a program that takes input from the user and when the user press enter
  to quit the program gives the sum of all even numbers and the sum of
  all odd numbers."""
-even = []
+even = [] #empty list
 odd = []
 while 1:
     x = input("Please enter a number and press enter or just enter to exit: ")
     if x == "":
         break
     elif int(x) % 2 == 0:
-        even.append(x)
+        even.append(x) #add number into even list
     else:
-        odd.append(x)
+        odd.append(x)  #add number into odd list
 
-print(even)
-print(odd)
-AddEven = 0
-AddOdd = 0
-for number in even:
-    AddEven = AddEven + int(number)
+print(even) #print the lists
+
+print(odd) #print the lists
+
+AddEven = 0 #blank numbers to count
+AddOdd = 0 #blank numbers to count
+for number in even: #
+    AddEven = AddEven + int(number) #add 1 to the count
 for number in odd:
-    AddOdd = AddOdd + int(number)
+    AddOdd = AddOdd + int(number) #add 1 to the count
 
-print("Sum of even number: ", str(AddEven))
-print("Sum of odd number: ", str(AddOdd))
+print("Sum of even number: ", str(AddEven)) #print results
+print("Sum of odd number: ", str(AddOdd)) #print results
