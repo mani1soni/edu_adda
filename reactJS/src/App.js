340226cc52e3f672618cf4838778c409ca338fca
Aqsa Umar <aksaumar35@gmail.com>
2020-10-12 10:48:43 +0500

    Add my-name to the list of contributors

---
+++ reactJS/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hellow React!!!!!!!!
+        </p>
+       
+          Learn React
+      
+      </header>
+    </div>
+  );
+}
+    
+export default App;
