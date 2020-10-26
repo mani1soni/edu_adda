7f0bb5eb7f90d3f1a64e0fdca30c54e6ddcdda9f
mani1soni <manish.soni8403@gmail.com>
2020-10-26 10:51:24 +0530

    files removed

--- devops/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "0.12.0"
-  backend "s3" {
-    region  = "us-east-1"
-    profile = "default"
-    key     = "newterraformstatefile"
-    bucket  = "pra-terraform-backend"
-  }
-}
